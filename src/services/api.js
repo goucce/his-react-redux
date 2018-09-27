72d3a997e8a0a04d941a3954c5506bee29fb86ed
goucce <sergiozcontacto@gmail.com>
2018-09-27 17:35:59 +0200

    inicio login con usuarios y roles

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,42 @@
-const users = localStorage.getItem( 'users' ) || [ {
-    role: 'doctor',
-    username: 'jupegarnica',
-    name: 'Juan',
-    password: '1234',
-    uid:'1'
-} ];
+const users = localStorage.getItem( 'users' ) || 
+[ {
+        role: 'admin',
+        uid:1,
+        name: 'Sergio',
+        surname: 'Zaragoza',
+        dni: '33765483F',
+        username: 'admin',
+        password: 'admin',
+    },
+
+    {
+        role: 'patient',
+        uid: 2,
+        name: 'Maria',
+        surname: 'Lan',
+        dni: '29965483F',
+        username: 'patient',
+        password: 'patient',
+    },
+    {
+        role: 'doctor',
+        uid: 3,
+        name: 'Laura',
+        surname: 'Laern',
+        dni: '29315483F',
+        username: 'doctor',
+        password: 'doctor',
+    },
+    {
+        role: 'technical',
+        uid: 4,
+        name: 'Paula',
+        surname: 'Garcia',
+        dni: '29965433V',
+        username: 'tecnico',
+        password: 'tecnico',
+    }
+ ];
 
 
 const api = {
