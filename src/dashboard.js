ee8421d36d3baa6d51df6f81ec4765ba05c3ae7a
goucce <sergiozcontacto@gmail.com>
2018-09-28 13:34:20 +0200

    dashboard medioexplicado

--- src/dashboard.js
+++ src/dashboard.js
@@ -5,21 +5,25 @@ import "./dashboard.css";
 
 class dashboard extends React.Component {
   
-  logout = () => {
-    this.props.logoutStore();
+  logout = () => {  // accedemos al props de logoutStore para que deje de estar registrado 
+    this.props.logoutStore(); //funcion que envia al reducer que  de deslogeará
   };
 
+  //lo que renderiza es en primere lugar
   render() {
-    if (!this.props.auth) {
+    if (!this.props.auth) { //que si no está logeado no nos dejepasar a dentro del dashboard
       return <Redirect to="/login" />;
-    } else {
-      const { role, name, uid } = this.props.auth;
 
+      //como si que estamos autentificados, nos henvia a un dashboard distinto segun el rol que tengamos
+    } else {
+      const { role, name, uid } = this.props.auth; //en propos.auth tenemos toda la informacion de los objetos, por lo tanto queremos las constantes role, name y uid.
+
+      //la constante links envia segun que lugar a un sito y a otro componente
       const links = [
         {
-          to: "/patientsList",
-          text: "Patients list",
-          roles: ["admin", "doctor"]
+          to: "/patientsList", //a donde nos lleva el link
+          text: "Patients list", //el texto del link
+          roles: ["admin", "doctor"] //los roles que pueden acceder a el
         },
         {
           to: "/histories",
@@ -32,7 +36,7 @@ class dashboard extends React.Component {
           roles: ["admin", "technical"]
         },
         {
-          to: "/patientDetail/"+ uid ,
+          to: "/patientDetail/"+ uid , //Cuando se accede aqui se añadirá el uid obtenido desde el this.props.auth.
           text: "Patients detail",
           roles: ["admin", "patient"]
         },
@@ -44,12 +48,15 @@ class dashboard extends React.Component {
       ];
 
       return (
+        // Devolvemos el HTML en efecto práctico.
         <section className="dashboard">
           <h1> Dashboard</h1>
 
           <h3>
-            Tu role es: {role}{" "}
-            {this.props.auth && <button onClick={this.logout}>Logout</button>}
+            Tu role es: {role}{" "} 
+            {/* Rol que recoge el auth y del usuario logeado */}
+            {this.props.auth && <button onClick={this.logout}>Logout</button>} 
+            {/* Click en el boten accede a la funcion logout del principio que te manda a la pagina inicial  */}
           </h3>
           <h5>
             {this.props.auth ? name + " is logged in." : "no user is logged in"}
@@ -77,7 +84,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    logoutStore: () =>
+    logoutStore: () => //enviamos al caso de logg out de dentro del reducer
       dispatch({
         type: "USER_LOGGED_OUT"
       })
